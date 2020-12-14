2ebf10049de485d17ff9f9b493c6fbfe35d4a04c
balbinxx <1815@holbertonschool.com>
2020-12-13 22:18:32 -0500

    99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i >= 0 and i < 10:
-        print('0{}, '.format(i), end='')
-    else:
-        print('{}, '.format(i), end='')
+for i in range(99):
+    print('{:02}, '.format(i), end='')
+print('{}'.format(i+1))
