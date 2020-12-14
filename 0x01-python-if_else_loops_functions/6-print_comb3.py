1884319b3fb69597e99414800f14f8f877257769
balbinxx <1815@holbertonschool.com>
2020-12-13 22:50:59 -0500

    89

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for x in range(10):
-        if i < x:
+        if i < x and (i != 8 or x != 9):
                 print('{}{}, '.format(i, x), end='')
+        if i == 8 and x == 9:
+            print('{}{}'.format(i, x))
