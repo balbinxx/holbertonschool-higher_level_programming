cf6288350cbe606e2bc9bb80c5d2d58c6677face
balbinxx <1815@holbertonschool.com>
2020-12-15 18:09:21 -0500

    inf add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys   
+    import sys
     add = 0
-    n = len(sys.argv)  
-    for i in range(1, n): 
-        add += int(sys.argv[i]) 
+    n = len(sys.argv)
+    for i in range(1, n):
+        add += int(sys.argv[i])
     print ('{}'.format(add))
