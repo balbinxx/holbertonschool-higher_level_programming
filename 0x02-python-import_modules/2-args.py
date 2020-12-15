775b8ae5830b70e832228052a1b9d584f0a3d699
balbinxx <1815@holbertonschool.com>
2020-12-15 17:31:11 -0500

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    x_len = len(sys.argv)
+    if x_len == 1:
+        print('{:d} arguments.'.format(x_len - 1))
+    elif x_len == 2:
+        print('{:d} argument:'.format(x_len - 1))
+        for i in range(x_len):
+            if i != 0:
+                print("{:d}: {:s}".format(i, sys.argv[i]))
+    else:  
+        print("{:d} arguments:".format(x_len - 1))
+        for i in range(x_len):
+            if i != 0:
+                print("{:d}: {:s}".format(i, sys.argv[i]))
