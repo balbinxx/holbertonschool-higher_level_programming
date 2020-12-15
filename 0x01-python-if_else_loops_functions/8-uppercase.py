529d2309cb0d0bf091c9b1588950c9d5e1d0eb4a
balbinxx <1815@holbertonschool.com>
2020-12-14 20:14:14 -0500

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in str:
         aux = ord(i)
-        if aux > 96 and aux < 123:       
+        if aux > 96 and aux < 123:
             aux -= 32
         print('{:c}'.format(aux), end='')
     print(end='\n')
