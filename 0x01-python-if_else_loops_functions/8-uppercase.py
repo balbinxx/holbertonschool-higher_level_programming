0032fc2b8ecd6478f1c5241f59d2c85b679ad7e2
balbinxx <1815@holbertonschool.com>
2020-12-14 20:10:31 -0500

    upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        aux = ord(i)
+        if aux > 96 and aux < 123:        
+            aux -= 32
+        print('{:c}'.format(aux), end='')
+    print(end='\n')
