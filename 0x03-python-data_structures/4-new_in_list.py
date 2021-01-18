ce0bc116b89ec0087a0884e21d7bd1146c14afbf
balbinxx <1815@holbertonschool.com>
2021-01-18 17:01:52 -0500

    replace in new

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    aux = my_list.copy()
+    if idx < 0 or idx + 1 > len(aux):
+        return aux
+    else:
+        aux [idx] = element
+        return aux
