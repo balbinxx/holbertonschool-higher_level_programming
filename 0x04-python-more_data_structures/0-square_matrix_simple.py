9843a15b746c3a4b7b0f6bfb5495acb4dc825813
balbinxx <1815@holbertonschool.com>
2021-01-19 12:10:47 -0500

    **2

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = matrix[:]
+    for i in range(len(new)):
+        new[i] = list(map(square, new[i]))
+    return new
+    
+def square(x):
+    return x ** 2
