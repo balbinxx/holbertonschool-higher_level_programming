0b97d7c2f9c942f1f6344b3adfc0b4a490ba6d41
balbinxx <1815@holbertonschool.com>
2021-01-18 19:06:30 -0500

    add tupla

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_c = (0, 0)
-    tuple_ac = tuple_a + tuple_c
-    tuple_bc = tuple_b + tuple_c
-    return (tuple_ac[0] + tuple_bc[0], tuple_ac[1] + tuple_bc[1])
+    c = (0, 0)
+    tupla_ac = tuple_a + c
+    tupla_bc = tuple_b + c
+    return (tupla_ac[0] + tupla_bc[0], tupla_ac[1] + tupla_bc[1])
