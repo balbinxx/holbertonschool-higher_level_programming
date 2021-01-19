910ab4c67e8c5ae25b3bab4b11983622531e7487
balbinxx <1815@holbertonschool.com>
2021-01-19 15:06:13 -0500

    common e

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for x in set_2:
-            if i is x or x is i:
-                return x or i
+    return set_1 & set_2
