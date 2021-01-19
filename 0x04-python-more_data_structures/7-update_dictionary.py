39b1283d7b44bcd1373abdf97171ae1a9794342a
balbinxx <1815@holbertonschool.com>
2021-01-19 16:19:52 -0500

    update dict

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    x = {key: value}
+    a_dictionary.update(x)
+    return (a_dictionary)
+
+
+def print_sorted_dictionary(new_dict):
+    for i in sorted(new_dict):   #imprimir dicts por elemento de forma "sort"
+        print("{}: {}".format(i, new_dict[i]))
