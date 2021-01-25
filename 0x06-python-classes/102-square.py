944e540a2f22c39f863cfd9f4bee770144778817
balbinxx <1815@holbertonschool.com>
2021-01-25 15:31:36 -0500

    1st

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+"""module."""
+
+
+class Square:
+    """AO."""
+
+    def __init__(self, size=0):
+        """Constructor."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for the length of a side of this square. """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """square.
+            size .
+        """
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
