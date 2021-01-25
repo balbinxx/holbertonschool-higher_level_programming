6e4778c68ec2c5d3588a8dc5d79d7b7169c157b6
balbinxx <1815@holbertonschool.com>
2021-01-25 16:34:09 -0500

    private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square"""
+class Square:
+    """the attribute (self.__size
+    have to have "__" to make it private
+    """
+    def __init__(self, size):
+        self.__size = size
