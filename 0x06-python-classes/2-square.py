fbaadd0f20a5825c5e1d2730e7e75a111de33866
balbinxx <1815@holbertonschool.com>
2021-01-26 16:18:23 -0500

    val of size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,9 +4,8 @@
 
 class Square:
     """validation of size"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         aux = size
-        
         if (type(aux) is not int):
             raise TypeError("size must be an integer")
         if (aux < 0):
