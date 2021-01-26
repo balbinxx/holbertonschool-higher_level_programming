866424ab484e990fb95e04cde6b4aa550ee36fac
balbinxx <1815@holbertonschool.com>
2021-01-26 18:25:04 -0500

    getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """constructor"""
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            
+    def area(self):
+        """"area return"""
+        return self.__size ** 2
+        
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+    
+    @size.setter
+    def size(self, aux):
+        """setter"""
+        if (type(aux) is not int):
+            raise TypeError("size must be an integer")
+        if (aux < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = aux
