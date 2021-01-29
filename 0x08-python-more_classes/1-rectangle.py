d5fc558ecf16e520d9c0f0e9c6997e8afe8e78f5
balbinxx <1815@holbertonschool.com>
2021-01-29 11:33:04 -0500

    pass

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,12 +7,12 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """getter"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """setter"""
@@ -22,12 +22,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-    
+
     @property
     def height(self):
         """getter"""
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         """setter"""
