6d8f45f9c868eeda9768283af37d3b8f3057b011
balbinxx <1815@holbertonschool.com>
2021-02-10 17:07:08 -0500

    load

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file"""
     with open(filename, "r", encoding="utf-8") as x:
-        json.load(x)
+        return json.load(x)
