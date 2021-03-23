22786006e93bd34f76a4d1ff758b410ef00cd759
balbinxx <1815@holbertonschool.com>
2021-03-22 20:32:22 -0500

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function factorial(n) {
-    if (!n) {
-        return 1; 
-    } else {
-        return n * factorial(n-1);
-    }
+function factorial (n) {
+  if (!n) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-let n = process.argv[2];
-let facto = factorial(n)
+const n = process.argv[2];
+const facto = factorial(n);
 console.log(facto);
