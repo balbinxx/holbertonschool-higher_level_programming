7f1529d17aab001ba3dde2c170d9cac44ae683e0
balbinxx <1815@holbertonschool.com>
2021-03-23 20:34:39 -0500

    tasj 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const NewList = [];
+  for (let x = list.length - 1; x >= 0; x--) {
+    NewList.push(list[x]);
+  }
+  return (NewList);
+}
+
