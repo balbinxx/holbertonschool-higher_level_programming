ecbf00b1f6ec9f7d1ec28952a3d5b4f0ffd7764b
balbinxx <1815@holbertonschool.com>
2021-03-27 17:14:15 -0500

    model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Random words bla
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class state(Base):
+    """ORM class"""
+    __tablename__ = 'states'
+    id = Column(Integer(), primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
