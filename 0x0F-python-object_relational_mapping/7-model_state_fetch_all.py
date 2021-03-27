14db3ac78088c7f4a871de990c2f08f18203f83f
balbinxx <1815@holbertonschool.com>
2021-03-27 18:32:25 -0500

    task 7 upd

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-session = Session(engine)
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    session = Session(engine)
+    for x in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(x.id, x.name))
+    session.close()
