f2802077c5f20074d66fa862a7f52353a375aff9
balbinxx <1815@holbertonschool.com>
2021-04-09 21:19:51 -0500

    first

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-""" Fetches basic request with urlib"""
+"""
+Fetches basic request with urlib
+"""
+
 from urllib import request
 
-with request.urlopen("https://intranet.hbtn.io/status") as response:
-    page = response.read()
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        page = response.read()
     print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
           .format(type(page), page, page.decode('utf-8')))
